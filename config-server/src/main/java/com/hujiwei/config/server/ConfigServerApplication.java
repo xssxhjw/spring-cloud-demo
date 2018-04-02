967beb156e95f9e90f60681c7ca1333e5b0c0266
hjw <770300402@qq.com>
2018-04-02 22:38:12 +0800

    spring-cloud-demo

--- config-server/src/main/java/com/hujiwei/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/hujiwei/config/server/ConfigServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+//@EnableDiscoveryClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
